cdb0756f75222a26dc1e7815455cf67d9235e21b
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-23 03:26:36 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,50 +1,45 @@
-def welcome
-  puts "Welcome to the Blackjack Table"
-end
-
-def deal_card
+def deal_card()
   rand(1..11)
 end
 
-def display_card_total(card_num)
-  puts "Your cards add up to #{card_num}"
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
+  card_total
 end
 
-def prompt_user
+def welcome()
+  puts "Welcome to the Blackjack Table"
+end
+
+def prompt_user()
   puts "Type 'h' to hit or 's' to stay"
 end
 
-def get_user_input
-  gets.chomp
+def initial_round()
+  sum = rand(1..11) + rand(1..11)
+  display_card_total(sum)
 end
 
 def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round
-  deal_card_1 = deal_card()
-  deal_card_2 = deal_card()
-
-  display_card_total(deal_card_1 + deal_card_2)
-  deal_card_1 + deal_card_2
+def get_user_input()
+  gets.chomp
 end
 
-def hit?(num)
-  total = num
-
+def hit?(card_total)
   prompt_user()
-  user_input = get_user_input()
-
-  if user_input == "h"
-    total += deal_card()
+  
+  if get_user_input == 'h'
+    card_total += deal_card
   end
-
-  total
+  
+  card_total
 end
 
-def invalid_command
-  # code invalid_command here
+def invalid_command()
+  puts "invalid command"
 end
 
 #####################################################
@@ -54,10 +49,11 @@ end
 def runner
   welcome
   card_total = initial_round
-
-  until card_total > 21
+  
+  while card_total < 21
     card_total = hit?(card_total)
     card_total = display_card_total(card_total)
   end
+  
   end_game(card_total)
 end
